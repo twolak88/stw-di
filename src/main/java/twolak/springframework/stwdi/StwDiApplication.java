a74ced19eaba1ff32aa9216ff83f171cf474c726
twolak88 <twolak88@gmail.com>
2020-08-08 19:11:45 +0200

    adding initial spring project

---
+++ src/main/java/twolak/springframework/stwdi/StwDiApplication.java
@@ -0,0 +1,13 @@
+package twolak.springframework.stwdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StwDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StwDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/twolak/springframework/stwdi/StwDiApplicationTests.java
@@ -0,0 +1,13 @@
+package twolak.springframework.stwdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StwDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
